3e9bd02c8e2d9e5d7022969360d779cad020536e
C-Shi <cshi@ualberta.ca>
2019-04-24 06:11:26 +0800

    reusable code in category display

--- include/sidebar.php
+++ include/sidebar.php
@@ -22,38 +22,20 @@
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
+                <?php
+                    $query = "SELECT * FROM categories";
+                    $query_select_all_categories = mysqli_query($connection, $query);
+                    while($row = mysqli_fetch_assoc($query_select_all_categories)) {
+                        $title = $row['title'];
+                        echo "<li><a href='#'>$title</a>";
+                    }
+                ?>
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
 
     <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
-
+    <?php include "widget.php"; ?>
 </div>
