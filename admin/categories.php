d96dc10e39579669321f1d1d9f6a045a28d008b6
C-Shi <cshi@ualberta.ca>
2019-04-24 12:38:34 +0800

    refactor admin code into functions

--- admin/categories.php
+++ admin/categories.php
@@ -1,4 +1,5 @@
 <?php include "include/admin_header.php" ?>
+<?php include "function.php" ?>
 
     <div id="wrapper">
 
@@ -18,24 +19,7 @@
                         </h1>
 
                         <!-- add category form -->
-                        <?php
-                            if(isset($_POST['submit'])) {
-                                $title = $_POST['title'];
-
-                                // validation for submit title
-                                if ($title === "" || empty($title)) {
-                                    echo "This field should not be empty";
-                                } else {
-                                    // adding verification for existing category
-                                    $query = "INSERT INTO categories(title) VALUES('{$title}')";
-                                    $query_create_category = mysqli_query($connection, $query);
-
-                                    if(!$query_create_category) {
-                                        die('Query Failed' . mysqli_error($connection));
-                                    }
-                                }
-                            }
-                        ?>
+                        <?php add_category() ?>
                         <div class="col-sm-6">
                             <!-- form for adding category -->
                             <form method="POST" action="">
@@ -50,33 +34,10 @@
 
                             <!-- form for edit category -->
                             <!-- update query for category title -->
-                            <?php
-                                if(isset($_POST['update'])) {
-                                    $title = $_POST['title'];
-                                    $id = $_POST['cat_id'];
-                                    $query = "UPDATE categories SET title = '{$title}' WHERE id = {$id};";
-                                    $update_cat_query = mysqli_query($connection, $query);
-                                    if (!$update_cat_query) {
-                                        die('Query Failed ' . mysqli_error($connection));
-                                    } else {
-                                        unset($_GET);
-                                    }
-                                }
-                            ?>
+                            <?php update_category() ?>
+
                             <form action="" method="POST">
-                                <?php
-                                    if(isset($_GET['edit'])) {
-                                        $edit_id = $_GET['edit'];
-                                        $query = "SELECT * FROM categories WHERE id = {$edit_id};";
-                                        $query_get_categories = mysqli_query($connection, $query);
-                                        $category = mysqli_fetch_assoc($query_get_categories);
-                                        echo "<div class='form-group'>";
-                                        echo "<input type='text' class='form-control' name='title' value={$category['title']}>";
-                                        echo "<input type='hidden' name='cat_id' value={$edit_id}>";
-                                        echo "</div>";
-                                        echo "<input type='submit' name='update' class='btn btn-primary' value='Update Category'>";
-                                    }
-                                ?>
+                                <?php show_edit() ?>
                             </form>
                         </div>
 
@@ -91,18 +52,7 @@
                                 </thead>
                                 <tbody>
                                     <!-- catch delete request -->
-                                    <?php
-                                        if(isset($_GET['delete'])) {
-                                            $cat_id = $_GET['delete'];
-                                            $query = "DELETE FROM categories WHERE id = {$cat_id};";
-                                            $query_delete_category = mysqli_query($connection, $query);
-                                            if(!$query_delete_category) {
-                                                die('Query Failed' . mysqli_error($connection));
-                                            } else {
-                                                header("Location: categories.php");
-                                            }
-                                        }
-                                    ?>
+                                    <?php delete_category() ?>
                                     <?php
                                         $query = "SELECT * FROM categories";
                                         $query_select_all_categories = mysqli_query($connection, $query);
---
+++ admin/function.php
@@ -0,0 +1,69 @@
+<?php
+
+function add_category() {
+    global $connection;
+    if(isset($_POST['submit'])) {
+      $title = $_POST['title'];
+
+      // validation for submit title
+      if ($title === "" || empty($title)) {
+          echo "This field should not be empty";
+      } else {
+          // adding verification for existing category
+          $query = "INSERT INTO categories(title) VALUES('{$title}')";
+          $query_create_category = mysqli_query($connection, $query);
+
+          if(!$query_create_category) {
+              die('Query Failed' . mysqli_error($connection));
+          }
+      }
+    }
+}
+
+function update_category() {
+  global $connection;
+  if(isset($_POST['update'])) {
+    $title = $_POST['title'];
+    $id = $_POST['cat_id'];
+    $query = "UPDATE categories SET title = '{$title}' WHERE id = {$id};";
+    $update_cat_query = mysqli_query($connection, $query);
+    if (!$update_cat_query) {
+        die('Query Failed ' . mysqli_error($connection));
+    } else {
+        unset($_GET);
+    }
+  }
+}
+
+function delete_category() {
+  global $connection;
+  if(isset($_GET['delete'])) {
+      $cat_id = $_GET['delete'];
+      $query = "DELETE FROM categories WHERE id = {$cat_id};";
+      $query_delete_category = mysqli_query($connection, $query);
+      if(!$query_delete_category) {
+          die('Query Failed' . mysqli_error($connection));
+      } else {
+          header("Location: categories.php");
+      }
+  }
+
+}
+
+function show_edit() {
+  global $connection;
+  if(isset($_GET['edit'])) {
+    $edit_id = $_GET['edit'];
+    $query = "SELECT * FROM categories WHERE id = {$edit_id};";
+    $query_get_categories = mysqli_query($connection, $query);
+    $category = mysqli_fetch_assoc($query_get_categories);
+    echo "<div class='form-group'>";
+    echo "<input type='text' class='form-control' name='title' value={$category['title']}>";
+    echo "<input type='hidden' name='cat_id' value={$edit_id}>";
+    echo "</div>";
+    echo "<input type='submit' name='update' class='btn btn-primary' value='Update Category'>";
+  }
+}
+
+
+?>
