24d573766fa60e6727d84031b1d8f7fdfbae7ed7
C-Shi <cshi@ualberta.ca>
2019-04-29 12:46:42 +0800

    display comments on post

--- function.php
+++ function.php
@@ -4,8 +4,26 @@
     global $connection;
     $query = "SELECT * FROM posts WHERE category_id = $cat_id";
     $query_find_posts = mysqli_query($connection, $query);
+    if (!$query_find_comments) {
+      die('Query Failed ' . mysqli_error($connection));
+    }
     $results = array();
     while ($row = mysqli_fetch_assoc($query_find_posts)) {
+      array_push($results, $row);
+    }
+    return $results;
+  }
+
+  function find_comments_by_post_id($post_id) {
+    global $connection;
+    $query = "SELECT * FROM comments WHERE post_id = $post_id;";
+    $query_find_comments = mysqli_query($connection, $query);
+
+    if (!$query_find_comments) {
+      die('Query Failed ' . mysqli_error($connection));
+    }
+    $results = array();
+    while ($row = mysqli_fetch_assoc($query_find_comments)) {
       array_push($results, $row);
     }
     return $results;
--- post.php
+++ post.php
@@ -1,5 +1,6 @@
 <?php include "include/db.php" ?>
 <?php include "include/header.php"; ?>
+<?php include "function.php" ?>
     <!-- Navigation -->
     <?php include "include/navigation.php"; ?>
 
@@ -96,44 +97,20 @@
                 <!-- Posted Comments -->
 
                 <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                    </div>
-                </div>
-
-                <!-- Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="http://placehold.it/64x64" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        <!-- Nested Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading">Nested Start Bootstrap
-                                    <small>August 25, 2014 at 9:30 PM</small>
-                                </h4>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                            </div>
+                <?php $comments = find_comments_by_post_id($post_id);?>
+                <?php foreach ($comments as $comment): ?>
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="http://placehold.it/64x64" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading"><?php echo $comment['author']?>
+                                <small><?php echo $comment['time']?></small>
+                            </h4>
+                            <?php echo $comment['content']?>
                         </div>
-                        <!-- End Nested Comment -->
                     </div>
-                </div>
-
+                <?php endforeach; ?>
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
